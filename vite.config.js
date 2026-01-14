878fdc3fabdc011d2ed11c477747295a3447d03c
Muneer Hadi <168365371+muneerhadi@users.noreply.github.com>
2026-01-14 11:57:17 +0430

    Add package-lock.json and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,8 @@ export default defineConfig({
         }),
     ],
     server: {
-        host: "0.0.0.0",
+        host: "localhost",
+        port: 5173,
         hmr: {
             host: "localhost",
         },
